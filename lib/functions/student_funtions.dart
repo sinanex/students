8b71277dcd4e13c194ad518732d1babb4c8bdd3e
sinanex <ms4730435@gmail.com>
2024-10-18 16:52:44 +0530

    home page updated

--- lib/functions/student_funtions.dart
+++ lib/functions/student_funtions.dart
@@ -5,18 +5,18 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:students/model/student_model.dart';
 
 ValueNotifier<List<Students>>studentListNotifier=ValueNotifier([]);
+
 Future<void>addStudent(Students value)async{
   final studentDb=await Hive.openBox<Students>('student_db');
   await studentDb.add(value);
    log(studentDb.name);
 getAllStudent();
-  
 }
 Future<void>getAllStudent()async{
 final studentDb=await Hive.openBox<Students>('student_db');
 studentListNotifier.value.clear();
 studentListNotifier.value.addAll(studentDb.values);
-  studentListNotifier.notifyListeners();
+studentListNotifier.notifyListeners();
 
 }
 
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,79 +16,73 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-
- String search = '';
+  String search = '';
   List<Students> searchedList = [];
   void searchListUpdate() {
     getAllStudent();
     searchedList = studentListNotifier.value
         .where((stdModel) =>
-          stdModel.studentName.toLowerCase().contains(search.toLowerCase()))
+            stdModel.studentName.toLowerCase().contains(search.toLowerCase()))
         .toList();
   }
 
   @override
   Widget build(BuildContext context) {
-getAllStudent();
+    getAllStudent();
     return Scaffold(
-        appBar: AppBar(
-          title:  SizedBox(
-            height: 50,
-            child: TextField(
-             
-                style: const TextStyle(color: Colors.white),
-                onChanged: (value) {
-                  setState(() {
-                    search = value;
-                    searchListUpdate();
-                  });
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  fillColor: Colors.white,
-                  labelText: 'Search',
-                  labelStyle: const TextStyle(color: Colors.white),
-                  focusColor: Colors.white,
-                  suffixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                ),
+      appBar: AppBar(
+        title: SizedBox(
+          height: 50,
+          child: TextField(
+            style: const TextStyle(color: Colors.white),
+            onChanged: (value) {
+              setState(() {
+                search = value;
+                searchListUpdate();
+              });
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
               ),
-          ),
-          
-          actions: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-              child: IconButton(
-                  onPressed: () {
-                    logout(context);
-                  
-                  },
-                  icon: Icon(Icons.logout)),
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.yellow[900],
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>AddDetails()));
-          },
-          child: Icon(Icons.add),
-        ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 20,
+              labelText: 'Search',
+              labelStyle: const TextStyle(color: Colors.white),
+              focusColor: Colors.white,
+              suffixIcon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
             ),
-            Expanded(
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+            child: IconButton(
+                onPressed: () {
+                  logout(context);
+                },
+                icon: Icon(Icons.logout)),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.yellow[900],
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddDetails()));
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
             child: ValueListenableBuilder(
               valueListenable: studentListNotifier,
               builder:
-                  // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
                   (BuildContext, List<Students> studentList, Widget? child) {
                 return search.isNotEmpty
                     ? searchedList.isEmpty
@@ -116,146 +110,143 @@ getAllStudent();
               style: TextStyle(color: Colors.white),
             ),
           )
-
-                      : ListView.builder(
-                          itemBuilder: (context, index) {
-                            final data = students[index];
-                            return ListTile(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => StudentsDetails(
-                                              name: data.studentName,
-                                              age: data.age,
-                                              cls: data.studentClass,
-                                              address: data.studentAddress,
-                                              imagePath: data.imagePath,
-                                            )));
-                              },
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) => Editpage(
-                                                      name: data.studentName,
-                                                      age: data.age,
-                                                      cls: data.studentClass,
-                                                      address: data.studentAddress,
-                                                      index: index,
-                                                      imagePath: data.imagePath,
-                                                    )));
-                                      },
-                                      icon: Icon(Icons.edit)),
-                                  IconButton(
-                                      onPressed: () {
-                                        deleteBtn(index, context);
-                                      },
-                                      icon: Icon(Icons.delete))
-                                ],
-                              ),
-                              leading: CircleAvatar(
-                                  // ignore: unnecessary_null_comparison
-                                  backgroundImage: data.imagePath != null ? FileImage(File(data.imagePath!)):AssetImage('assets/images/std.webp')
-                               
-                              // backgroundImage: FileImage(File(data.imagePath!))
-                              ),
-                              
-                              title: Text(
-                                data.studentName,
-                                style: TextStyle(color: Colors.amber),
-                              ),
-                            );
-                          },
-                          itemCount: students.length,
-                        );
-                }
-        //       ),
-        //     ),
-        //   ],
-        // ));
-  }
-
-
-  void deleteBtn(int index, BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Container(
-            height: 100,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("Conform Delete?", style: TextStyle(fontSize: 18)),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              final data = students[index];
+              return ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => StudentsDetails(
+                                name: data.studentName,
+                                age: data.age,
+                                cls: data.studentClass,
+                                address: data.studentAddress,
+                                imagePath: data.imagePath,
+                              )));
+                },
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text("Cancel"),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        deleteStudent(index);
-                        Navigator.of(context).pop();
-                      },
-                      child: Text("Delete"),
-                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Editpage(
+                                        name: data.studentName,
+                                        age: data.age,
+                                        cls: data.studentClass,
+                                        address: data.studentAddress,
+                                        index: index,
+                                        imagePath: data.imagePath,
+                                      )));
+                        },
+                        icon: Icon(Icons.edit)),
+                    IconButton(
+                        onPressed: () {
+                          deleteBtn(index, context);
+                        },
+                        icon: Icon(Icons.delete))
                   ],
                 ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
+                leading: CircleAvatar(
+                    // ignore: unnecessary_null_comparison
+                    backgroundImage: data.imagePath != null
+                        ? FileImage(File(data.imagePath!))
+                        : AssetImage('assets/images/std.webp')
 
-
-  void logout( BuildContext context) async{
-       SharedPreferences pref = await SharedPreferences.getInstance();
-    pref.setBool('log', false);
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Container(
-            height: 100,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("Conform Logout?", style: TextStyle(fontSize: 18)),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text("Cancel"),
+                    // backgroundImage: FileImage(File(data.imagePath!))
                     ),
-                    TextButton(
-                      onPressed: () {
-                       Navigator.pushReplacementNamed(context, '/log');
-                      },
-                      child: Text("Logout"),
-                    ),
-                  ],
+                title: Text(
+                  data.studentName,
+                  style: TextStyle(color: Colors.amber),
                 ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
+              );
+            },
+            itemCount: students.length,
+          );
   }
+  //       ),
+  //     ),
+  //   ],
+  // ));
+}
 
+void deleteBtn(int index, BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: Container(
+          height: 100,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Conform Delete?", style: TextStyle(fontSize: 18)),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Cancel"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      deleteStudent(index);
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Delete"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+void logout(BuildContext context) async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  pref.setBool('log', false);
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: Container(
+          height: 100,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Conform Logout?", style: TextStyle(fontSize: 18)),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Cancel"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/log');
+                    },
+                    child: Text("Logout"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
